8beab25de31f5358af6d26edf4fae7f2a1793a74
Jitesh Jain <66688144+jiteshjain17@users.noreply.github.com>
2022-02-04 18:02:40 +0530

    Optimized solution

--- HashMaps/525. ContiguousArray.cpp
+++ HashMaps/525. ContiguousArray.cpp
@@ -22,26 +22,45 @@ public:
     // O(n) using hashmap:
 // https://leetcode.com/problems/contiguous-array/discuss/577279/C%2B%2B-Crystal-Clear-Explanation
         
-        int ans = 0, n = nums.size();
+//         int ans = 0, n = nums.size();
+//         unordered_map<int,int> mp;
+//         int sum = 0;
+        
+//         for(int i = 0; i<n; i++){
+            
+//             sum += (nums[i] == 0) ? -1 : 1;
+            
+//             auto it = mp.find(sum);
+            
+//             if(sum == 0){
+//                 ans = i+1;
+//             }
+//             else if(it != mp.end()){
+//                 ans = max(ans, i - it->second);
+//             }
+//             else if(it == mp.end()){
+//                 mp.insert({sum,i});
+//             }
+//         }
+//         return ans;
+        
+        
+    // More simpler solution:
+        
+        int n = nums.size();
         unordered_map<int,int> mp;
-        int sum = 0;
+        int count = 0, maxlen = 0;
+        
+        mp[0] = -1;        // for edge case [0,1]
         
         for(int i = 0; i<n; i++){
-            
-            sum += (nums[i] == 0) ? -1 : 1;
-            
-            auto it = mp.find(sum);
-            
-            if(sum == 0){
-                ans = i+1;
-            }
-            else if(it != mp.end()){
-                ans = max(ans, i - it->second);
-            }
-            else if(it == mp.end()){
-                mp.insert({sum,i});
+            count += (nums[i]==0) ? -1 : 1;
+            if(mp.find(count) != mp.end()){
+                maxlen = max(maxlen, i-mp[count]);
+            }else{
+                mp[count] = i;
             }
         }
-        return ans;
+        return maxlen;
     }
 };
