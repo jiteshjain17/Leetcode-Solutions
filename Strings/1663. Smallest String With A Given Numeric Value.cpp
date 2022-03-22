e29d37469a24af6e7bfc69d3b6fe22f96baf5219
Jitesh Jain <66688144+jiteshjain17@users.noreply.github.com>
2022-03-22 12:53:51 +0530

    Create 1663. Smallest String With A Given Numeric Value.cpp

---
+++ Strings/1663. Smallest String With A Given Numeric Value.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        
+        string res(n, 'a');
+        k -= n;
+        
+        while(k > 0){
+            res[--n] += min(25,k);
+            k -= min(25,k);
+        }
+        return res;
+    }
+};
+
+// https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/discuss/1871662/JavaC%2B%2B-Easiest-Possible-Exaplained!!
